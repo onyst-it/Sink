04ac13e9a2a3591545ccedb7cb826cc008c267c9
onyst-it <102585459+onyst-it@users.noreply.github.com>
2026-03-07 20:46:41 -0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
-  slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
+  slugRegex: /^[a-zA-Z0-9_-]+$/,
   reserveSlug: [
     'dashboard',
   ],
